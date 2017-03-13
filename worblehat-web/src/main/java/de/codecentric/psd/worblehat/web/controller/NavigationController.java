b7197b28756715cf1118a6967c938f153604e8b1
js8464s <julia.seikrit@alumni.fh-aachen.de>
2017-03-13 16:46:31 +0200

    js | added version in view

--- worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/NavigationController.java
+++ worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/NavigationController.java
@@ -1,6 +1,7 @@
 package de.codecentric.psd.worblehat.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
